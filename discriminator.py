e17438f2ae23bd8d5d67a47e8118d33d0b2a3f82
LostGeorge <george_hu@brown.edu>
2023-05-28 00:38:07 -0400

    add base discriminator code and domain confusion loss

---
+++ discriminator.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from typing import List, Callable
+
+class MLPDiscriminator(nn.Module):
+    def __init__(
+            self,
+            encoder_dim: int,
+            hidden_dims: List[int],
+            n_domains: int = 3,
+            act_fn: Callable = nn.GELU,
+            ) -> None:
+        super().__init__()
+        assert len(hidden_dims) >= 1
+        layers = [
+            nn.Linear(encoder_dim, hidden_dims[0]),
+            act_fn()
+        ]
+        for i in range(1, len(hidden_dims)):
+            layers.append(nn.Linear(hidden_dims[i-1], hidden_dims[i]))
+            layers.append(act_fn())
+        layers.append(nn.Linear(hidden_dims[-1], n_domains))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, enc_out):
+        return self.model(enc_out)
+
+
+
