8441e8960fc38d0d09e56aefe1f90e926db35461
JacobASmith <14015973+JacobASmith@users.noreply.github.com>
2023-05-28 21:31:18 -0400

    Prototype plMLP

--- sar_main.py
+++ sar_main.py
@@ -4,7 +4,7 @@ by Boehm et. al 2022, NIPS workshop on Tackling Climate Change with Machine Lear
 https://github.com/iprapas/landslide-sar-unet
 '''
 
-from sar_unet_src.lit_module import plUNET
+from sar_unet_src.lit_module import plUNET, plMLP
 from sar_unet_src.datamodule import BeforeAfterCubeDataModule
 import pytorch_lightning as pl
 import torch
@@ -22,6 +22,14 @@ def add_plUNET_args(parent_parser):
     parser.add_argument("--loss", type=str, default='ce')
     return parent_parser
 
+# a function to read plUNET parameters as program arguments
+def add_plMLP_args(parent_parser):
+    parser = parent_parser.add_argument_group("plUNET")
+    parser.add_argument("--lr", type=float, default=0.01)
+    parser.add_argument("--weight_decay", type=float, default=0.0001)
+    parser.add_argument("--loss", type=str, default='nse')
+    return parent_parser
+
 
 def add_BeforeAfterCubeDataModule_args(parent_parser):
     parser = parent_parser.add_argument_group("BeforeAfterCubeDataModule")
@@ -31,7 +39,7 @@ def add_BeforeAfterCubeDataModule_args(parent_parser):
     parser.add_argument("--event_start_date", type=str, default='20180905')
     parser.add_argument("--event_end_date", type=str, default='20180907')
     parser.add_argument("--input_vars", type=str, default='vv_before,vv_after,vh_before,vh_after')
-    # parser.add_argument("--target", type=str, default='landslides')
+    parser.add_argument("--target", type=str, default='landslides')
     # parser.add_argument("--train_val_test_split", type=str, default='0.7_0.2_0.1')
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--num_workers", type=int, default=0)
@@ -49,49 +57,73 @@ if __name__ == '__main__':
     # add argument for max_epochs
     parser.add_argument("--max_epochs", type=int, default=10)
     # parse the arguments
-    hparams = parser.parse_args()
+    unet_hparams = parser.parse_args()
+
+     # create the top-level parser
+    parser = argparse.ArgumentParser()
+    # add arguments
+    parser = add_plMLP_args(parser)
+    parser = add_BeforeAfterCubeDataModule_args(parser)
+    # add argument for max_epochs
+    parser.add_argument("--max_epochs", type=int, default=10)
+    mlp_hparams = parser.parse_args()
 
     # transform ba_vars to list
-    hparams.ba_vars = hparams.ba_vars.split(',')
+    unet_hparams.ba_vars = unet_hparams.ba_vars.split(',')
     # transform input_vars to list
-    hparams.input_vars = hparams.input_vars.split(',')
+    unet_hparams.input_vars = unet_hparams.input_vars.split(',')
     # transform train_val_test_split to tuple
     # hparams.train_val_test_split = tuple([float(x) for x in hparams.train_val_test_split.split('_')])
 
 
     # run the main function
-    print(hparams)
+    print(unet_hparams)
+    print(mlp_hparams)
     utils.seed_everything(0)
     # create the datamodule from the appropriate parsed arguments
     dm = BeforeAfterCubeDataModule(
-        ds_path=hparams.ds_path,
-        ba_vars=hparams.ba_vars,
+        ds_path=unet_hparams.ds_path,
+        ba_vars=unet_hparams.ba_vars,
         aggregation='mean',
-        timestep_length=hparams.timestep_length,
-        event_start_date=hparams.event_start_date,
-        event_end_date=hparams.event_end_date,
-        input_vars=hparams.input_vars,
+        timestep_length=unet_hparams.timestep_length,
+        event_start_date=unet_hparams.event_start_date,
+        event_end_date=unet_hparams.event_end_date,
+        input_vars=unet_hparams.input_vars,
         target='landslides',
         train_val_test_split=(0.7, 0.2, 0.1),
-        batch_size=hparams.batch_size,
+        batch_size=unet_hparams.batch_size,
         include_negatives=False,
-        num_workers=hparams.num_workers
+        num_workers=unet_hparams.num_workers
     )
 
     # create the plUNET from the appropriate parsed arguments
     model = plUNET(
-        lr=hparams.lr,
-        weight_decay=hparams.weight_decay,
-        num_channels=len(hparams.input_vars),
-        loss=hparams.loss
+        lr=unet_hparams.lr,
+        weight_decay=unet_hparams.weight_decay,
+        num_channels=len(unet_hparams.input_vars),
+        loss=unet_hparams.loss
     )
+    # model2 = plMLP(
+    #     encoder=model.encoder,
+    #     lr=mlp_hparams.lr,
+    #     weight_decay=mlp_hparams.weight_decay,
+    #     num_channels=len(mlp_hparams.input_vars),
+    #     loss=mlp_hparams.loss
+    # )
 
     # create the trainer with the appropriate parsed arguments
-    trainer = pl.Trainer(max_epochs=hparams.max_epochs)
+    trainer = pl.Trainer(max_epochs=unet_hparams.max_epochs)
 
     # train the model
     trainer.fit(model, datamodule=dm)
     trainer.validate(datamodule=dm)
     trainer.test(datamodule=dm)
-    if hparams.save_path is not None:
-        torch.save(model.state_dict(), hparams.save_path)
+    if unet_hparams.save_path is not None:
+        torch.save(model.state_dict(), unet_hparams.save_path)
+
+    # train the model
+    # trainer.fit(model2, datamodule=dm)
+    # trainer.validate(datamodule=dm)
+    # trainer.test(datamodule=dm)
+    # if mlp_hparams.save_path is not None:
+    #     torch.save(model2.state_dict(), mlp_hparams.save_path)
--- sar_unet_src/datamodule.py
+++ sar_unet_src/datamodule.py
@@ -21,7 +21,7 @@ def before_after_ds(ds_path, ba_vars, aggregation, timestep_length, event_start_
     ds = xr.open_zarr(ds_path)
     for var in ba_vars:
         ds[var] = np.log(ds[var])
-    orbit_state_bool_arr = (ds['sat:orbit_state'] == 'd').compute()
+    orbit_state_bool_arr = (ds['sat_orbit_state'] == 'd').compute()
     ds = ds.where(orbit_state_bool_arr, drop=True)
     before_ds = ds.drop_dims('timepair').sel(timestep=slice(None, event_start_date))
     after_ds = ds.drop_dims('timepair').sel(timestep=slice(event_end_date, None))
